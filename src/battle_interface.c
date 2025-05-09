d97293227cb6010869f28d28ad021db7fe438aaf
Maateria <mnouville1@outlook.fr>
2025-05-09 14:00:40 +0200

    [FIX] - Fix Lv by N.

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -765,7 +765,7 @@ static void UpdateLvlInHealthbox(u8 healthboxSpriteId, u8 lvl)
 {
     u32 windowId, spriteTileNum;
     u8 *windowTileData;
-    u8 text[16] = _("{LV_2}");
+    u8 text[16] = _("N.");
     u32 xPos;
     u8 *objVram;
 
